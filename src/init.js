17694a5f35e622aedec4ba55bdc4f55a4ee2111a
Glitch (strickinato-elm-dojo-1) <none>
2019-10-03 07:44:15 +0000

    🍵🌒 Checkpoint ./src/init.js:1594177/1432

--- src/init.js
+++ src/init.js
@@ -1,23 +1,20 @@
 import { Elm } from './Main.elm'
 import * as CryptoJS from "crypto-js"
 
-const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN
-const PASSCODE = process.env.PASSCODE
-
- 
-// Encrypt
-const ciphertext = CryptoJS.AES.encrypt(PASSCODE, SLACK_API_TOKEN);
- 
-
- 
+const ENCRYPTED_SLACK_API_TOKEN = process.env.ENCRYPTED_SLACK_API_TOKEN 
 
 const app = Elm.Main.init({
     node: document.getElementById("mount"),
-    flags: SLACK_API_TOKEN,
+    flags: ENCRYPTED_SLACK_API_TOKEN,
 });
 
-app.ports.decrypt.subscribe(data => {
-  const bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-  const plaintext = bytes.toString(CryptoJS.enc.Utf8);
-  console.log(plaintext)
+app.ports.decrypt.subscribe(secret => {
+  const bytes  = CryptoJS.AES.decrypt(ENCRYPTED_SLACK_API_TOKEN, secret);
+  try {
+    const plaintext = bytes.toString(CryptoJS.enc.Utf8)
+    app.ports.getDecrypted.send(plaintext)
+  } catch {
+    console.error('no biggy')
+  }
+  
 });
