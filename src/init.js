0853df9c7578c09a53a8f51b749ee884e0448357
Glitch (strickinato-elm-dojo-1) <none>
2019-10-10 19:55:30 +0000

    🐋🦄 Checkpoint ./src/init.js:1594177/39

--- src/init.js
+++ src/init.js
@@ -3,14 +3,21 @@ import * as CryptoJS from "crypto-js"
 
 const ENCRYPTED_SLACK_API_TOKEN = process.env.ENCRYPTED_SLACK_API_TOKEN 
 
+
+
 const app = Elm.Main.init({
     node: document.getElementById("mount"),
     flags: ENCRYPTED_SLACK_API_TOKEN,
 });
 
+
+
+
+
+
 app.ports.decrypt.subscribe(secret => {
   try {
-    
+    console.log('decrypting secret')
     const bytes     = CryptoJS.AES.decrypt(ENCRYPTED_SLACK_API_TOKEN, secret);
     const plaintext = bytes.toString(CryptoJS.enc.Utf8)
     
